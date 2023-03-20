ad84a28c8a23a36d10b3c40e39bdbd349757a203
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-03-20 18:23:24 -0300

    feat> new font added

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -9,7 +9,7 @@ export const CategoryItem = ({ category }) => {
         style={{ backgroundImage: `url(${imageUrl})` }}
       ></div>
       <div className="category-body-container">
-        <h2>{title}</h2>
+        <h2>{title.toUpperCase()}</h2>
         <p>Shop Now</p>
       </div>
     </div>
