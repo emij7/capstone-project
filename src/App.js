34f0a69ed7e106de5eeb2eaa2516112f65ffbc9c
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-03-23 17:07:50 -0300

    feat: shop and products components with styles added.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { Routes, Route } from "react-router-dom";
 import React from "react";
 import { Navigation } from "./routes/navigation/navigation.component";
 import { Authentication } from "./components/authentication/authentication.component";
+import { Shop } from "./routes/shop/shop.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Navigation />} />
+        <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { ProductCard } from "../../components/product-card/product-card.component";
+import { ProductContext } from "../../contexts/products.context";
+import "./shop.styles.scss";
+
+export const Shop = () => {
+  const { products } = useContext(ProductContext);
+  return (
+    <div className="products-container">
+      {products.map((product) => (
+        <ProductCard key={products.id} product={product} />
+      ))}
+    </div>
+  );
+};
