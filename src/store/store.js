9fd035ac06da953cc02cb3b28907ca2038046a59
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-04-10 16:35:29 -0300

    feat: redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { categoriesSlice } from "./categories/categoriesSlice";
 import { userSlice } from "./user/userSlice";
 import { cartSlice } from "./cart/cartSlice";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
 
-//root-reducer
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user", "cart"],
+};
 
-export const store = configureStore({
-  reducer: {
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
     user: userSlice.reducer,
     categories: categoriesSlice.reducer,
     cart: cartSlice.reducer,
-  },
+  })
+);
+
+export const store = configureStore({
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
+//root-reducer
+
+// export const store = configureStore({
+//   reducer: {
+//     user: userSlice.reducer,
+//     categories: categoriesSlice.reducer,
+//     cart: cartSlice.reducer,
+//   },
+// });
