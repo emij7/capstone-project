072e54f97e2420ebf6d57530db0322275bcb4ad5
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2023-04-11 17:55:47 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { persistor, store } from "./store/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
